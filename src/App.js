9fdac851f250e915b6b58f37a910e29fe6ab0b96
VenkatRamReddyK <venkatramreddykunta@gmail.com>
2021-09-28 15:32:05 -0700

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './style.css';
-import ClickEvents from './ClickEvents';
+// import ClickEvents from './ClickEvents';
 // import PieChart from './PieChart';
+import PieChartWithEvents from './PieChartWithEvents';
 export default function App() {
   return (
     <div className="chart-container">
       {/* <PieChart /> */}
-      <ClickEvents />
+      <PieChartWithEvents />
+      {/* <ClickEvents /> */}
     </div>
   );
 }
