a7857e51aeb540d0d53d83324f850d5efbf07e94
VenkatRamReddyK <venkatramreddykunta@gmail.com>
2021-09-28 15:28:29 -0700

    Added Pie Chart and Click Events.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
-import "./style.css";
-
+import React from 'react';
+import './style.css';
+import ClickEvents from './ClickEvents';
+// import PieChart from './PieChart';
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="chart-container">
+      {/* <PieChart /> */}
+      <ClickEvents />
     </div>
   );
 }
